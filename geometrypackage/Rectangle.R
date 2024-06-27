86cfac5ddc554c5a42bb84ffcf6e131f35bc4e85
safiraferia <safiraamalina85@gmail.com>
2024-06-27 13:40:35 +0700

    Add files via upload

---
+++ geometrypackage/Rectangle.R
@@ -0,0 +1,13 @@
+#' Perimeter and area of a rectangle
+#'
+#' @param p Length of the rectangle
+#' @param l Width of the rectangle
+#'
+#' @return A list with perimeter and area
+#' @export
+rectangle <- function(p, l) {
+  perimeter <- 2 * (p + l)
+  area <- p * l
+  list(perimeter = perimeter, area = area)
+}
+rectangle(4, 15)
